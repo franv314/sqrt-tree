4a415bdab62ab1088506cd8c9e5024aa8788dedf
Francesco Vercellesi <francesco@vercellesi.com>
2024-09-13 13:09:10 +0200

    Added sqrt tree in sqrt tree.

--- src/sqrt_tree.cpp
+++ src/sqrt_tree.cpp
@@ -1,45 +1,61 @@
 #include <vector>
 #include <functional>
 #include <cmath>
+#include <type_traits>
 using namespace std;
 
-template<typename T, typename F = plus<T>>
+template<typename T, typename F = plus<T>, bool Indexed = true>
 class SqrtTree {
+
+    template<bool Top = false>
     struct Item {
         int block_size;
         vector<T> suffix, prefix;
-        vector<vector<T>> between;
+        conditional_t<Top, SqrtTree<T, F, false>, vector<vector<T>>> between;
 
         T query(int l, int r, const F& f) const {
             int fst_block = l / block_size;
             int lst_block = r / block_size;
 
-            if (lst_block > fst_block + 1)
-                return f(f(suffix[l], between[fst_block + 1][lst_block - 1]), prefix[r]);
+            if (lst_block > fst_block + 1) {
+                if constexpr (Top)
+                    return f(f(suffix[l], between.query(fst_block + 1, lst_block)), prefix[r]);
+                else
+                    return f(f(suffix[l], between[fst_block + 1][lst_block - 1]), prefix[r]);
+            }
             return f(suffix[l], prefix[r]);
         }
 
-
-        Item() { }
-        Item(typename vector<T>::iterator start, typename vector<T>::iterator end, int block_size, const F& f)
-            : block_size(block_size), suffix(start, end), prefix(start, end)
-        {
-            for (int i = 0; i < end - start; i += block_size) {
+        decltype(between) build(const F& f) {
+            for (int i = 0; i < prefix.size(); i += block_size) {
                 for (int j =  1; j < block_size; j++)
                     prefix[i + j] = f(prefix[i + j - 1], prefix[i + j]);
                 for (int j = block_size - 2; j >= 0; j--)
                     suffix[i + j] = f(suffix[i + j + 1], suffix[i + j]);
             }
 
-            int blocks = (end - start) / block_size;
-            between.assign(blocks, vector<T>(blocks));
+            int blocks = prefix.size() / block_size;
+            vector<T> block_vals(block_size);
+            for (int i = 0; i < blocks; i++)
+                block_vals[i] = suffix[i * block_size];
 
-            for (int i = 0; i < blocks; i++) {
-                between[i][i] = suffix[i * block_size];
-                for (int j = i + 1; j < blocks; j++)
-                    between[i][j] = f(between[i][j - 1], suffix[j * block_size]);
+            if constexpr (Top) {
+                return SqrtTree<T, F, false>(block_vals, f);
+            } else {
+                vector<vector<T>> bet(blocks, vector<T>(blocks));
+
+                for (int i = 0; i < blocks; i++) {
+                    bet[i][i] = block_vals[i];
+                    for (int j = i + 1; j < blocks; j++)
+                        bet[i][j] = f(bet[i][j - 1], block_vals[j]);
+                }
+
+                return bet;
             }
         }
+
+        Item(typename vector<T>::iterator start, typename vector<T>::iterator end, int block_size, const F& f)
+            : block_size(block_size), suffix(start, end), prefix(start, end), between(std::move(build(f))) { }
     };
 
     static constexpr int get_layer[] = {
@@ -55,33 +71,7 @@ class SqrtTree {
     static constexpr int get_layer_shift[] = { 2, 4, 8, 16 };
     static constexpr unsigned int get_layer_mask[] = { 0x3, 0xf, 0xff, 0xffff, 0xffffffff };
 
-    F f;
-    vector<T> arr;
-    vector<vector<Item>> layers;
-
-public:
-
-    T query(int l, int r) {
-        switch (r - l) {
-        case 0: return T();
-        case 1: return arr[l];
-        case 2: return f(arr[l], arr[l + 1]);
-        default:
-            r--;
-            int layer = get_layer[64 - __builtin_clzll(l ^ r)];
-
-            if (layer == layers.size() - 1)
-                return layers[layer][0].query(l, r, f);
-
-            int block = l >> get_layer_shift[layer];
-            l &= get_layer_mask[layer];
-            r &= get_layer_mask[layer];
-
-            return layers[layer][block].query(l, r, f);
-        }
-    }
-
-    SqrtTree(const vector<T> &arr, const F &f) : arr(arr), f(f) {
+    Item<Indexed> build() {
         int s = 1 << (int)ceil(log2(arr.size()));
 
         vector<T> tmp = arr;
@@ -92,15 +82,45 @@ public:
             int curr_size = 1 << curr;
             int block_size = 1 << old;
 
-            vector<Item> layer(s >> curr);
+            vector<Item<>> layer;
+            layer.reserve(s >> curr);
+
             for (int i = 0; i < s; i += curr_size)
-                layer[i >> curr] = Item(tmp.begin() + i, tmp.begin() + i + curr_size, block_size, f);
+                layer.emplace_back(tmp.begin() + i, tmp.begin() + i + curr_size, block_size, f);
 
             layers.push_back(std::move(layer));
         }
 
-        layers.push_back({Item(tmp.begin(), tmp.end(), 1 << old, f)});
+        return Item<Indexed>(tmp.begin(), tmp.end(), 1 << old, f);
     }
 
+    F f;
+    vector<T> arr;
+    vector<vector<Item<>>> layers;
+    Item<Indexed> top_layer;
+
+public:
+
+    T query(int l, int r) const {
+        switch (r - l) {
+        case 0: return T();
+        case 1: return arr[l];
+        case 2: return f(arr[l], arr[l + 1]);
+        default:
+            r--;
+            int layer = get_layer[64 - __builtin_clzll(l ^ r)];
+
+            if (layer == layers.size())
+                return top_layer.query(l, r, f);
+
+            int block = l >> get_layer_shift[layer];
+            l &= get_layer_mask[layer];
+            r &= get_layer_mask[layer];
+
+            return layers[layer][block].query(l, r, f);
+        }
+    }
+
+    SqrtTree(const vector<T> &arr, const F &f) : arr(arr), f(f), top_layer(std::move(build())) { }
     SqrtTree(const vector<T> &arr) : SqrtTree(arr, F()) { }
 };
